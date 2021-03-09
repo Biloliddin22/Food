cf1915a4fb5cee98505ca4ca3105a4ea2debfdf4
Biloliddin <biloliddin500dbmin@gmail.com>
2021-03-09 21:52:30 +0500

    add slider

--- js/script.js
+++ js/script.js
@@ -339,8 +339,59 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-     fetch('http://localhost:3000/menu')
+     /* fetch('http://localhost:3000/menu')
     .then(data => data.json())
-    .then(res => console.log(res)); 
+    .then(res => console.log(res)); */ 
+
+    // Slider
+
+    const prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current'),
+          slides = document.querySelectorAll('.offer__slide');
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.classList.add('hide'));
+
+        slides[slideIndex - 1].classList.remove('hide');
+        slides[slideIndex - 1].classList.add('show');
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
 
 });
